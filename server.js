c39e3dfd6f23ac7dd44ab3fd8d344e4bfa7cb9d1
C0RT3XGIT <cortexchemodan@gmail.com>
2022-12-28 22:13:37 +0200

    Cors origin updated

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ connectDB().then((response)=> response);
 
 
 app.use(express.urlencoded({ extended: false }))
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.CORS_ORIGIN
+}));
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use('/api', clientRoutes);
